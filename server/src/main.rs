fb8234ada358b907d80f6a2269d89be61b2f15e3
William Lundstedt <bananavice@gmail.com>
2018-01-28 17:49:58 +0000

    Some progress

--- server/src/main.rs
+++ server/src/main.rs
@@ -3,8 +3,8 @@ extern crate ws;
 fn main() {
     ws::listen("127.0.0.1:3012", |out| {
         move |msg| {
-            println!("Got message: {}", msg);
-            Ok(())
+            println!("Received message: {}", msg);
+            out.send(msg)
         }
     }).unwrap()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 #![feature(set_stdio)]
+#![feature(conservative_impl_trait)]
 
+mod websocket;
+
+use std::cell::RefCell;
 use std::mem;
 use std::ffi::CString;
+use std::ptr::null_mut;
 use std::os::raw::{c_char, c_void};
 use std::io;
 
+use websocket::WebSocket;
+type MainLoopCallback = unsafe extern "C" fn();
+
 extern "C" {
     fn write(ptr: *const u8);
-    fn websocket_create(url_ptr: *const u8) -> u32;
-    fn websocket_send(handle: u32, data_ptr: *const u8) -> bool;
+
+    fn set_main_loop(fn_ptr: MainLoopCallback);
 }
 
 // In order to work with the memory we expose (de)allocation methods
@@ -59,22 +67,56 @@ impl std::io::Write for ConsoleWriter {
     }
 }
 
+thread_local!(static MAIN_LOOP_CALLBACK: RefCell<*mut c_void> = RefCell::new(null_mut()));
+
+fn set_main_loop_callback<T>(callback: T)
+where
+    T: FnMut(),
+{
+    MAIN_LOOP_CALLBACK
+        .with(|cb| *cb.borrow_mut() = Box::into_raw(Box::new(callback)) as *mut c_void);
+
+    pub unsafe extern "C" fn wrapper<T>()
+    where
+        T: FnMut(),
+    {
+        MAIN_LOOP_CALLBACK.with(|cb| {
+            let callback = *cb.borrow_mut() as *mut T;
+            (*callback)()
+        })
+    }
+
+    unsafe {
+        set_main_loop(wrapper::<T>);
+    }
+}
+
+fn main() {}
+
 #[no_mangle]
 pub extern "C" fn run() {
     io::set_print(Some(Box::new(ConsoleWriter(Vec::new()))));
     io::set_panic(Some(Box::new(ConsoleWriter(Vec::new()))));
 
-    println!("Running client");
-    let url = CString::new("ws://localhost:3012").unwrap();
-    let socket;
-    unsafe {
-        println!("Creating websocket");
-        socket = websocket_create(url.into_raw() as *const u8);
-    }
-    println!("Created socket with handle: {}", socket);
+    let mut socket = WebSocket::connect("ws://localhost:3012").unwrap();
+    let mut ping = true;
+    let mut x = 1;
+    let update = || {
+        if socket.open() {
+            if ping {
+                let msg = format!("{}", x);
+                println!("Ping: {}", x);
+                socket.send(msg.as_bytes()).unwrap();
+                ping = false;
+                x += 1;
+            }
 
-    let message = CString::new("Testing sending a message").unwrap();
-    unsafe {
-        websocket_send(socket, message.into_raw() as *const u8);
-    }
+            while let Some(msg) = socket.next() {
+                println!("Pong: {}", msg);
+                ping = true;
+            }
+        }
+    };
+
+    set_main_loop_callback(update);
 }
